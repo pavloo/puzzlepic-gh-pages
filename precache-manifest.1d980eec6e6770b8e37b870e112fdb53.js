343ead3c2e40f8c77d2ec1c3805131593e671a61
pavloo <posadchiy@gmail.com>
2022-11-17 23:17:02 -0800

    Updates

---
+++ precache-manifest.1d980eec6e6770b8e37b870e112fdb53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "581992462c003ee0a003",
+    "url": "/puzzlepic-gh-pages/static/css/main.d143a5bf.chunk.css"
+  },
+  {
+    "revision": "581992462c003ee0a003",
+    "url": "/puzzlepic-gh-pages/static/js/main.58199246.chunk.js"
+  },
+  {
+    "revision": "31d6fcf003e49cef91ec",
+    "url": "/puzzlepic-gh-pages/static/js/1.31d6fcf0.chunk.js"
+  },
+  {
+    "revision": "3ed3d3ba64e23ea16f47",
+    "url": "/puzzlepic-gh-pages/static/js/runtime~main.3ed3d3ba.js"
+  },
+  {
+    "revision": "5992eb52cbe2856dca683a956837bd53",
+    "url": "/puzzlepic-gh-pages/index.html"
+  }
+];
